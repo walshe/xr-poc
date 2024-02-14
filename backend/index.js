c4e4dc1486e017dbdc7aae5005c1f1c5739dc6c8
Emmett Walsh <emmettwalsh@gmail.com>
2024-02-14 19:10:53 +0700

    improve

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ app.use(cors());
 const { Magic } = require('@magic-sdk/admin');
 
 // Initialize Magic with your secret key
-const magic = new Magic('sk_live_6ADF3B2FAB1FF4EE');
+const magic = new Magic('sk_live_3A4CC4E6E340E38D');
+const moralisApiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjYwYmEyZWEwLTc0MWItNGY5MS1iNmFkLTk0NTE5MjQ3OWRjMCIsIm9yZ0lkIjoiOTY1OSIsInVzZXJJZCI6Ijk2NCIsInR5cGUiOiJQUk9KRUNUIiwidHlwZUlkIjoiODJiOTkwNzYtODA1YS00MjdjLTg1NDUtZjg0YjcyMzkzZWVlIiwiaWF0IjoxNzA3OTA5NjMxLCJleHAiOjQ4NjM2Njk2MzF9.n_9c-v7WC3sv_vKhaDL0peEu9hL3FL-p-1GnsH-_ovU';
 
 
 // Authentication middleware - this verifies the did token ssent in from the frontent to secure any backend business logic related to that user
@@ -34,52 +35,72 @@ const authenticateWithMagic = async (req, res, next) => {
     }
 };
 
-app.get('/api/some-secure-endpoint', authenticateWithMagic, async (req, res) => {
-    // Your secure endpoint logic here
-    res.json({
-        message: 'This is secure info from the server for this logged in user!!'
-    })
+// Secure endpoint to fetch balance
+app.get('/api/fetch-balance', authenticateWithMagic, async (req, res) => {
+    const walletAddress = req.magicMetadata.publicAddress; // Assuming the public address is available in user metadata
+    const moralisApiUrl = `https://deep-index.moralis.io/api/v2.2/${walletAddress}/balance?chain=eth`;
 
 
+    console.log(moralisApiUrl)
+
+    try {
+        const response = await axios.get(moralisApiUrl, {
+            headers: {
+                accept: 'application/json',
+                'X-API-Key': moralisApiKey,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching balance data from Moralis:', error.message);
+        res.status(500).json({ message: 'Failed to fetch NFT data', error: error.message });
+    }
 });
 
 // Secure endpoint to fetch NFT data
 app.get('/api/fetch-nfts', authenticateWithMagic, async (req, res) => {
     const walletAddress = req.magicMetadata.publicAddress; // Assuming the public address is available in user metadata
-    const moralisApiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjI1ZWNiMWFjLTBhYzUtNGFkOS1hYmQ1LThkNTRhN2MzZTFjZSIsIm9yZ0lkIjoiOTY1OSIsInVzZXJJZCI6Ijk2NCIsInR5cGVJZCI6IjgyYjk5MDc2LTgwNWEtNDI3Yy04NTQ1LWY4NGI3MjM5M2VlZSIsInR5cGUiOiJQUk9KRUNUIiwiaWF0IjoxNzA3ODE4NjA0LCJleHAiOjQ4NjM1Nzg2MDR9._S5tPvGuALAOrV2IpXi2J2laBmHwS8XwDLIaNBjgQIs'
     const moralisApiUrl = `https://deep-index.moralis.io/api/v2.2/${walletAddress}/nft?chain=eth&format=decimal&media_items=false`;
-                           
+
 
     console.log(moralisApiUrl)
-  
+
     try {
-      const response = await axios.get(moralisApiUrl, {
-        headers: {
-          accept : 'application/json',  
-          'X-API-Key': moralisApiKey,
-        },
-      });
-      res.json(response.data);
+        const response = await axios.get(moralisApiUrl, {
+            headers: {
+                accept: 'application/json',
+                'X-API-Key': moralisApiKey,
+            },
+        });
+        res.json(response.data);
     } catch (error) {
-      console.error('Error fetching NFT data from Moralis:', error.message);
-      res.status(500).json({ message: 'Failed to fetch NFT data', error: error.message });
+        console.error('Error fetching NFT data from Moralis:', error.message);
+        res.status(500).json({ message: 'Failed to fetch NFT data', error: error.message });
     }
-  });
+});
 
 
-app.get('/api/crypto', async (req, res) => {
+//public
+app.get('/api/top-nft-collections', async (req, res) => {
     try {
-        // const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets', {
-        //   params: {
-        //     vs_currency: 'usd',
-        //     order: 'market_cap_desc',
-        //     per_page: 10,
-        //     page: 1,
-        //     sparkline: false,
-        //   },
-        // });
-        // res.json(response.data);
-        res.json([{ id: 'BTC', name: 'Bitcoin', symbol: 'BTC', current_price: '50000' }])
+
+        const moralisApiUrl = `https://deep-index.moralis.io/api/v2.2/market-data/nfts/top-collections`;
+
+        console.log(moralisApiUrl)
+
+        try {
+            const response = await axios.get(moralisApiUrl, {
+                headers: {
+                    accept: 'application/json',
+                    'X-API-Key': moralisApiKey,
+                },
+            });
+            res.json(response.data);
+        } catch (error) {
+            console.error('Error fetching NFT data from Moralis:', error.message);
+            res.status(500).json({ message: 'Failed to fetch NFT data', error: error.message });
+        }
+        
     } catch (error) {
         console.error('Error fetching cryptocurrency data:', error);
         res.status(500).json({ message: 'Error fetching data' });
--- readme.md
+++ readme.md
@@ -2,19 +2,21 @@
 
 Bare bones crude frontend and backend POC (Ethereum for now):
 
-    - Authentication with MagicLink (goofy workflow for now until add Google social login on top)
+    - Authentication with MagicLink (workflow is goofy for now until Google/other social login added on top)
     - By default uses Magic Custodial wallet
     - Allows external wallet to be connected with Web3Modal
-    - Uses Moralis API's to list any NFTs in the wallet
+    - Uses Moralis API's to fetch top NFT collections
+    - Uses Moralis API's to get baklance and list any NFTs in the wallet 
     - Allows user to sign a simple message with the current wallet (whether it custodial or external)
 
 
     - backend (run this first)
         - uses Node v18.17.0
-        - run npm install
-        - run npm start (backend starts at http://localhost:3001)
+        - run `npm install`
+        - run `node index.js` (backend starts at http://localhost:3001)
+         
 
     - frontend
         - uses Node v18.17.0
-        - run npm install
-        - run npm start (front end will start at http://localhost:3000)
+        - run `npm install`
+        - run `npm start` (front end will start at http://localhost:3000)
